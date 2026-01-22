53dda7b0fafdbf97309e4043bba1689d95c66bd2
wmb81321 <william.martinez@example.com>
2026-01-22 18:22:32 -0500

    updating team

--- js/about.js
+++ js/about.js
@@ -2,160 +2,39 @@
 console.log('About-simple.js loaded with complete team data');
 
 const teamData = [
-    {
-        name: "William Martinez",
-        role: "Economist, Project Manager, Web3 Hacker",
-        description: "Core member desde Octubre 2022. Visionario y líder del ecosistema Ethereum Cali.",
-        status: "Core",
-        image: "team/william.png",
-        linkedin: "https://www.linkedin.com/in/williammartinez8/",
-        twitter: "https://twitter.com/0xwmb",
-        github: "https://github.com/wmb81321"
-    },
-    {
-        name: "Juan Urrea",
-        role: "Researcher, Web3 Hacker",
-        description: "Core member desde Octubre 2022. Investigador y hacker especializado en Web3.",
-        status: "Core",
-        image: "team/Juanurrea.png",
-        linkedin: "",
-        twitter: "https://twitter.com/juan21179",
-        github: ""
-    },
-    {
-        name: "Filadelfo Caicedo",
-        role: "Economist, Trader, Web3 Researcher",
-        description: "Core member desde Octubre 2023. Economista y trader especializado en investigación Web3.",
-        status: "Core",
-        image: "team/filadelfo.png",
-        linkedin: "https://www.linkedin.com/in/filadelfo-caicedo/",
-        twitter: "https://twitter.com/Filatrader",
-        github: "https://github.com/phill900"
-    },
-    {
-        name: "Maria del Mar Borrero",
-        role: "Community Manager, Digital Marketing",
-        description: "Core member desde Febrero 2024. Gestora de comunidad y especialista en marketing digital.",
-        status: "Core",
-        image: "team/maria del mar.png",
-        linkedin: "https://www.linkedin.com/in/mariadelmarborrerog/",
-        twitter: "https://twitter.com/marimarketingw3",
-        github: ""
-    },
-    {
-        name: "Jose Bailon",
-        role: "WEB3 Developer",
-        description: "Core member desde Agosto 2023. Desarrollador especializado en tecnologías Web3.",
-        status: "Core",
-        image: "team/juan jose bailon.png",
-        linkedin: "https://linkedin.com/in/juan-bailon-ab3767192",
-        twitter: "",
-        github: "https://github.com/juanbailon"
-    },
-    {
-        name: "Juan Esteban Sierra",
-        role: "Business Developer, NFT Strategy",
-        description: "Core member desde Diciembre 2022. Desarrollador de negocios y estratega NFT.",
-        status: "Core",
-        image: "team/juan esteban sierra.png",
-        linkedin: "https://www.linkedin.com/in/juanfteth/",
-        twitter: "https://twitter.com/juaNFT_ETH",
-        github: ""
-    },
-    {
-        name: "Cristobal Valencia",
-        role: "Full Stack Developer",
-        description: "Core member desde Abril 2025. Desarrollador full stack del ecosistema.",
-        status: "Core",
-        image: "team/cristobal.jpg",
-        linkedin: "https://www.linkedin.com/in/cristobalvalenciaceron",
-        twitter: "https://x.com/DevCristobalvc",
-        github: "https://github.com/DevCristobalvc"
-    },
-    {
-        name: "Juan David Burgos",
-        role: "Lawyer",
-        description: "Core member desde Mayo 2025. Asesor legal especializado en tecnologías blockchain.",
-        status: "Core",
-        image: "team/juan david burgos.jpg",
-        linkedin: "https://www.linkedin.com/in/juan-david-burgos-giraldo-0000000000",
-        twitter: "https://x.com/JuanBK_ethcali",
-        github: ""
-    },
-    {
-        name: "Saeta Florez",
-        role: "DAO and Talent Manager",
-        description: "DAO coordinator desde Mayo 2025. Desarrollador web3 especializado en tecnologías blockchain.",
-        status: "Core",
-        image: "team/saeta.png",
-        linkedin: "https://www.linkedin.com/in/saeta7/",
-        twitter: "https://x.com/Hortelana_eth",
-        github: ""
-    },
-    {
-        name: "Miguel Bolaños",
-        role: "Front End Developer",
-        description: "Core member desde Noviembre 2023. Desarrollador frontend especializado en interfaces Web3.",
-        status: "Contribuidor",
-        image: "team/miguel bolanos.png",
-        linkedin: "",
-        twitter: "https://twitter.com/MAB015",
-        github: ""
-    },
-    {
-        name: "Adriana Lucema",
-        role: "Project Manager, Web3 Product Manager",
-        description: "Contribuidora desde Mayo 2024. Gestora de proyectos y productos Web3.",
-        status: "Contribuidor",
-        image: "team/adriana_lucema.png",
-        linkedin: "https://www.linkedin.com/in/adriana-lucema/",
-        twitter: "https://twitter.com/AdriLucema",
-        github: ""
-    },
-    {
-        name: "Jose Luis Rivas",
-        role: "Educador, Web3 Hacker",
-        description: "Contribuidor desde Enero 2023. Educador y hacker especializado en Web3.",
-        status: "Contribuidor",
-        image: "team/joseluisrivas.png",
-        linkedin: "https://www.linkedin.com/in/jlrivasv/",
-        twitter: "https://twitter.com/cointaigas",
-        github: ""
-    },
-    {
-        name: "Camilo Sacanamboy",
-        role: "Entrepreneur, WEB3 Developer",
-        description: "Contribuidor desde Enero 2023. Emprendedor y desarrollador Web3.",
-        status: "Contribuidor",
-        image: "team/camilosacanamboy.png",
-        linkedin: "https://www.linkedin.com/in/camilosaka/",
-        twitter: "https://twitter.com/camilosaka",
-        github: ""
-    },
-    {
-        name: "Andres Jimenez",
-        role: "Entrepreneur",
-        description: "Contribuidor desde Octubre 2022. Emprendedor del ecosistema.",
-        status: "Contribuidor",
-        image: "team/andresjimenez.png",
-        linkedin: "https://www.linkedin.com/in/andresfjimenez/",
-        twitter: "https://twitter.com/Andresjimenezv",
-        github: ""
-    },
-    {
-        name: "Camilo Tobar",
-        role: "Entrepreneur, Web3 Hacker",
-        description: "Contribuidor desde Octubre 2022. Emprendedor y hacker Web3.",
-        status: "Contribuidor",
-        image: "team/camilotobar.png",
-        linkedin: "https://www.linkedin.com/in/camilotobarn/",
-        twitter: "",
-        github: ""
-    }
+    { name: "William Martinez", role: "Founder, Economist, Project Manager", status: "Founder", since: "01/10/2022", image: "team/william.png", linkedin: "https://www.linkedin.com/in/williammartinez8/", twitter: "https://twitter.com/0xwmb", github: "https://github.com/wmb81321" },
+    { name: "Juan Urrea", role: "Researcher, Web3 Hacker", status: "Founder", since: "01/10/2022", image: "team/Juanurrea.png", linkedin: "", twitter: "https://twitter.com/juan21179", github: "" },
+    { name: "Filadelfo Caicedo", role: "Economist, Trader, Web3 Researcher", status: "Core", since: "01/10/2023", image: "team/filadelfo.png", linkedin: "https://www.linkedin.com/in/filadelfo-caicedo/", twitter: "https://twitter.com/Filatrader", github: "https://github.com/phill900" },
+    { name: "Maria del Mar Borrero", role: "Community Manager, Digital Marketing", status: "Core", since: "01/02/2024", image: "team/mariadelmar.png", linkedin: "https://www.linkedin.com/in/mariadelmarborrerog/", twitter: "https://twitter.com/marimarketingw3", github: "" },
+    { name: "Jose Bailon", role: "Back-end Developer", status: "Core", since: "01/08/2023", image: "team/juan jose bailon.png", linkedin: "https://linkedin.com/in/juan-bailon-ab3767192", twitter: "", github: "https://github.com/juanbailon" },
+    { name: "Cristobal Valencia", role: "FullStack Developer", status: "Contributor", since: "01/04/2025", image: "team/cristobal.jpg", linkedin: "https://www.linkedin.com/in/cristobalvalenciaceron", twitter: "https://x.com/DevCristobalvc", github: "https://github.com/DevCristobalvc" },
+    { name: "Juan David Burgos", role: "Lawyer", status: "Contributor", since: "01/05/2025", image: "team/juan david burgos.jpg", linkedin: "https://www.linkedin.com/in/juan-david-burgos-cantor-1603b3196/", twitter: "https://x.com/JuanBK_ethcali", github: "https://github.com/orgs/Ekinoxis-evm/people/juandavid883" },
+    { name: "Luis Waitoto", role: "Web2 dev in course + logistics coordinator", status: "Volunteer", since: "01/11/2025", image: "team/luiswaitoto.jpg", linkedin: "https://www.linkedin.com/in/luiswaitoto-/", twitter: "", github: "" },
+    { name: "John Alex", role: "Web2 dev in course", status: "Volunteer", since: "01/11/2025", image: "team/jhonalex.jpg", linkedin: "https://www.linkedin.com/in/jhonnalexandertorrescastro/", twitter: "", github: "https://github.com/orgs/Ekinoxis-evm/people/DevJhonnTorres" },
+    { name: "Camilo Ramirez", role: "Fullstack developer", status: "Volunteer", since: "01/11/2025", image: "team/camiloramirez.png", linkedin: "https://www.linkedin.com/in/camilo-ram%C3%ADrez-villegas/", twitter: "", github: "" },
+    { name: "Susan Taborda", role: "Web2 Dev in course", status: "Volunteer", since: "01/11/2025", image: "team/susantaborda.png", linkedin: "https://www.linkedin.com/in/susan-taborda/", twitter: "https://x.com/tabordasusan", github: "" },
+    { name: "Miguel Bolanos", role: "Front-end and UXUI dev", status: "Volunteer", since: "01/11/2025", image: "team/miguelbolanos.png", linkedin: "https://www.linkedin.com/in/mab015/", twitter: "https://twitter.com/MAB015", github: "https://github.com/MAB015/MAB015" },
+    { name: "Laura Castro", role: "Lawyer and Customer Success", status: "Volunteer", since: "01/11/2025", image: "team/lauracastro.jpg", linkedin: "https://www.linkedin.com/in/launacg", twitter: "", github: "" },
+    { name: "Camilo Sacanamboy", role: "Founder, WEB3 Developer", status: "Volunteer", since: "01/01/2023", image: "team/camilosacanamboy.png", linkedin: "https://www.linkedin.com/in/camilosaka/", twitter: "https://twitter.com/camilosaka", github: "https://github.com/csacanam" },
+    { name: "Juan Esteban Sierra", role: "Business Developer, NFT Strategy", status: "Former Core", since: "01/12/2022", image: "team/juan esteban sierra.png", linkedin: "https://www.linkedin.com/in/juanfteth/", twitter: "https://twitter.com/juaNFT_ETH", github: "" },
+    { name: "Saeta Florez", role: "DAO and Talent Manager", status: "Former Core", since: "01/05/2025", image: "team/saeta.png", linkedin: "https://www.linkedin.com/in/saeta7/", twitter: "https://x.com/Hortelana_eth", github: "" },
+    { name: "Adriana Lucema", role: "Project Manager, Web3 Product Manager", status: "Former Core", since: "01/06/2023", image: "team/adriana_lucema.png", linkedin: "https://www.linkedin.com/in/adriana-lucema/", twitter: "https://twitter.com/AdriLucema", github: "" },
+    { name: "Jose Luis Rivas", role: "Educador, Web3 Hacker", status: "Former Core", since: "01/01/2023", image: "team/joseluisrivas.png", linkedin: "https://www.linkedin.com/in/jlrivasv/", twitter: "https://twitter.com/cointaigas", github: "" },
+    { name: "Andres Jimenez", role: "Founder", status: "Former Core", since: "01/10/2022", image: "team/andresjimenez.png", linkedin: "https://www.linkedin.com/in/andresfjimenez/", twitter: "https://twitter.com/Andresjimenezv", github: "" },
+    { name: "Camilo Tobar", role: "Founder, Web3 Hacker", status: "Former Core", since: "01/10/2022", image: "team/camilotobar.png", linkedin: "https://www.linkedin.com/in/camilotobarn/", twitter: "", github: "" }
 ];
 
 function createTeamCard(member) {
-    const statusClass = member.status.toLowerCase();
+    // Assign color class based on status
+    let statusClass = "";
+    switch (member.status) {
+        case "Founder": statusClass = "founder"; break;
+        case "Core": statusClass = "core"; break;
+        case "Contributor": statusClass = "contributor"; break;
+        case "Volunteer": statusClass = "volunteer"; break;
+        case "Former Core": statusClass = "former-core"; break;
+        default: statusClass = "other";
+    }
     
     // Create social links
     let socialLinks = '';
@@ -188,9 +67,9 @@ function createTeamCard(member) {
                 <div class="card-info">
                     <h3>${member.name}</h3>
                     <div class="card-role">${member.role}</div>
+                    <div class="card-since">Desde: ${member.since}</div>
                 </div>
             </div>
-            <p class="card-description">${member.description}</p>
             <div class="card-social">
                 ${socialLinks}
             </div>
